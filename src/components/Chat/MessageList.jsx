bd9effecce3caca295ae9dae4ec18ce88648ca56
excellent <flarpzz08@gmail.com>
2024-12-14 22:09:27 +0100

    small ui fix

--- src/components/Chat/MessageList.jsx
+++ src/components/Chat/MessageList.jsx
@@ -3,13 +3,12 @@ import { formatTime } from "../../timeFormatter.js";
 import Avatar from "./Avatar.jsx";
 
 import recieverPic from "../../assets/recieverPic.png";
-import senderPic from "../../assets/senderPic.png";
 
 //dummy data
 const CURRENT_USER = {
   id: "7",
-  name: "You",
-  avatar: senderPic,
+  name: "Me",
+  avatar: recieverPic,
 };
 
 export default function MessageList({ messages }) {
@@ -17,32 +16,30 @@ export default function MessageList({ messages }) {
 
   if (!isMyMessage) {
     return (
-      <div className="mb-2 flex flex-col">
-        <div className="mb-2 flex gap-3">
-          <Avatar size="40" userPic={messages[0].senderAvatar} />
-          <div className="flex flex-col gap-2">
-            <div className="flex gap-1">
-              <p className="text-[14px] font-bold leading-5 text-white">
-                {messages[0].senderName}
-              </p>
-              <span className="text-secondary text-xs font-medium leading-5">
-                ·
-              </span>
-              <p className="text-secondary text-xs font-medium leading-5">
-                {formatTime(messages[0].createdAt)}
-              </p>
-            </div>
-            {messages.map((message) => {
-              return (
-                <p
-                  className="break-all text-[14px] font-normal leading-5 text-white"
-                  key={message.id}
-                >
-                  {message.text}
-                </p>
-              );
-            })}
+      <div className="mb-4 flex gap-3">
+        <Avatar size="40" userPic={messages[0].senderAvatar} />
+        <div className="flex flex-col gap-2 overflow-x-auto">
+          <div className="flex gap-1">
+            <p className="text-[14px] font-bold leading-5 text-white">
+              {messages[0].senderName}
+            </p>
+            <span className="text-secondary text-xs font-medium leading-5">
+              ·
+            </span>
+            <p className="text-secondary text-xs font-medium leading-5">
+              {formatTime(messages[0].createdAt)}
+            </p>
           </div>
+          {messages.map((message) => {
+            return (
+              <p
+                className="break-words text-[14px] font-normal leading-5 text-white"
+                key={message.id}
+              >
+                {message.text}
+              </p>
+            );
+          })}
         </div>
       </div>
     );
@@ -50,9 +47,9 @@ export default function MessageList({ messages }) {
   if (isMyMessage) {
     return (
       <div className="mb-2 flex flex-col gap-3">
-        <div className="mb-2 flex flex-row gap-3">
+        <div className="mb-2 flex gap-3">
           <Avatar size="40" userPic={messages[0].senderAvatar} />
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 overflow-x-auto">
             <div className="flex flex-row gap-1">
               <p className="text-[14px] font-bold leading-5 text-white">
                 {messages[0].senderName}
@@ -67,7 +64,7 @@ export default function MessageList({ messages }) {
             {messages.map((message) => {
               return (
                 <p
-                  className="text-wrap break-all rounded-lg text-[14px] font-normal leading-5 text-white"
+                  className="break-words rounded-lg text-[14px] font-normal leading-5 text-white"
                   key={message.id}
                 >
                   {message.text}
